881b200721a6904504023d3513ac5b789865c665
Liam O'Shaughnessy <liamoshaughn@gmail.com>
2024-11-11 16:41:27 +1100

    Added gambit score and end game screen

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import './App.css';
 import Battle from './components/Battle';
 import Missions from './components/Missions';
 import Rules from './components/Rules';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useStore } from '../src/store/store';
 
 function App() {
   const turn = useStore((state) => state.turn);
-  const [phase, setPhase] = useState(turn >1 ? 1 : 3);
+  const [phase, setPhase] = useState(1); // Default phase while loading
 
+
+  useEffect(() => {
+    // Set initial phase based on turn value from the store
+    setPhase(turn >= 1 ? 3 : 1);
+  }, [turn]);
 
   return (
     <div className="App">
       <header className="App-header">
       </header>
-        <div style={{display:"flex", justifyContent:"center", textAlign:"left"}}>
-          {phase === 1 && <Rules nextPhase={()=>setPhase(2)}/>}
-          {phase === 2 && <Missions nextPhase={()=>setPhase(3) }/>}
-          {phase === 3 && <Battle/>}
-        </div>
+      <div style={{ display: "flex", justifyContent: "center", textAlign: "left" }}>
+        {phase === 1 && <Rules nextPhase={() => setPhase(2)} />}
+        {phase === 2 && <Missions nextPhase={() => setPhase(3)} />}
+        {phase === 3 && <Battle />}
+      </div>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Battle.js
+++ src/components/Battle.js
@@ -16,15 +16,14 @@ export default function Battle(props) {
   const [currentSecondary, setCurrentSecondary] = useState(0);
   const [gambitTime, setGambitTime] = useState(false);
   const [endGame, setEndGame] = useState(false);
+  const [gambitSuccess, setGambitSuccess] = useState(null);
 
   const handleDiscard = (discard) => {
     store.setSecondary(secondary.filter((mission) => mission !== discard));
   };
 
   const handleNext = () => {
-    if (turn == 5) {
-      setEndGame(true)
-    }
+
     if (turn == 2) {
       setGambitTime(true);
     }
@@ -38,7 +37,9 @@ export default function Battle(props) {
 
     store.setSecondaryScore(Math.min(newSecondaryScore, maxSecondaryScore));
     setCurrentSecondary(0);
-
+    if (turn >= 4) {
+      setEndGame(true)
+    }
     store.setTurn();
     handleDrawMission();
   };
@@ -69,92 +70,109 @@ export default function Battle(props) {
 
   return (
     <div style={{ width: '100vw', height: 'fit-content' }}>
-      {gambitTime && !gambit ? (
-        <Gambit />
-      ) : (
-        <div style={{ display: 'flex', gap: '3%', flexWrap: 'wrap' }}>
-          <div style={{ width: '100%', maxWidth: '500px' }}>
-            <h2>Battle - Turn {turn + 1}</h2>
-            <h3>Current Missions</h3>
-            <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
-              {secondary.map((mission, index) => (
-                <MissionCard key={index} index={index} mission={mission} handleDiscard={() => handleDiscard(mission)} />
-              ))}
-            </div>
+      {endGame ? (<>
+        {gambit.name !== 'Proceed as Planned' && gambitSuccess === null ? (
+          <div>
+            <h2>Was the gambit completed successfully?</h2>
+            <button onClick={() => {setGambitSuccess(true);    localStorage.clear()}}>Yes</button>
+            <button onClick={() => {setGambitSuccess(true);    localStorage.clear()}}>No</button>
           </div>
-          <div style={{ width: '100%', maxWidth: '300px' }}>
-            <h2>Score {primaryScore + secondaryScore}</h2>
-            <div>
-              <h3>Primary Score</h3>
-              <input
-                disabled={gambit && gambit?.name != 'Proceed as Planned' ? true : false}
-                value={currentPrimary}
-                onChange={(e) => setCurrentPrimary(e.target.value)}
-              />
-            </div>
-            <div>
-              <h3>Secondary Score</h3>
-              <input value={currentSecondary} onChange={(e) => setCurrentSecondary(e.target.value)} />
-            </div>
-            <div style={{ marginTop: '20px' }}>
-              <button onClick={() => handleNext()}>Next Turn</button>
-            </div>
-            <div>
-              <p>Make sure to score and discard secondaries before clicking next if they have been achieved</p>
-            </div>
-          </div>
-          <div style={{ width: '100%', maxWidth: '500px' }}>
-            <div>
-              {gambit && gambit.name != 'Proceed as Planned' ? (
-                <>
-                  <h4>
-                    If Gambit successful add this to your score: <b> {Math.min(30, 50 - primaryScore)} VP</b>
-                  </h4>
-                  <h3>Gambit</h3>
-                  <h4>{gambit.name}</h4>
-                  <p>
-                    <strong>Gambit Mission:</strong> <br />
-                    {gambit.gambit}
-                  </p>
-                </>
-              ) : (
-                <>
-                  <h3>Primary Mission</h3>
-                  <h4>{store.mission.name}</h4>
-                  <p>
-                    <strong>Primary Mission Rule:</strong> <br />
-                    {store.mission.special}
-                  </p>
-                  <p>
-                    <strong>Scoring for current round:</strong> <br /> {store.mission.battle_rounds[turn]}
-                  </p>
-                </>
-              )}
-            </div>
-            <div>
-              <h4>
-                <strong>Mission Rule</strong>
-              </h4>
-              <div style={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
-                {store.rule.map((mission, index) => {
-                  return (
-                    <div key={index} style={{ border: '1px solid white', padding: '20px', flex: '1' }}>
-                      <h3>{mission.name}</h3>
-                      <div style={{ fontSize: '12px' }}>
-                        <p>{mission.rule}</p>
-                      </div>
-                    </div>
-                  );
-                })}
+        ) : (
+          <h2>
+            Final Score: {(() => {
+              const gambitScore = gambitSuccess ? Math.min(30, 50 - primaryScore) : 0;
+              return primaryScore + secondaryScore + gambitScore;
+            })()}
+          </h2>
+
+        )}
+      </>) : (<>
+        {gambitTime && !gambit ? (
+          <Gambit />
+        ) : (
+          <div style={{ display: 'flex', gap: '3%', flexWrap: 'wrap' }}>
+            <div style={{ width: '100%', maxWidth: '500px' }}>
+              <h2>Battle - Turn {turn + 1}</h2>
+              <h3>Current Missions</h3>
+              <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
+                {secondary.map((mission, index) => (
+                  <MissionCard key={index} index={index} mission={mission} handleDiscard={() => handleDiscard(mission)} />
+                ))}
               </div>
             </div>
-            <div>
-              <h3>Deployment</h3>
-              <img style={{ maxWidth: '300px' }} alt="Deployment Zone" src={store.deployment.image} />
+            <div style={{ width: '100%', maxWidth: '300px' }}>
+              <h2>Score {primaryScore + secondaryScore}</h2>
+              <div>
+                <h3>Primary Score</h3>
+                <input
+                  disabled={gambit && gambit?.name != 'Proceed as Planned' ? true : false}
+                  value={currentPrimary}
+                  onChange={(e) => setCurrentPrimary(e.target.value)}
+                />
+              </div>
+              <div>
+                <h3>Secondary Score</h3>
+                <input value={currentSecondary} onChange={(e) => setCurrentSecondary(e.target.value)} />
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <button onClick={() => handleNext()}>Next Turn</button>
+              </div>
+              <div>
+                <p>Make sure to score and discard secondaries before clicking next if they have been achieved</p>
+              </div>
+            </div>
+            <div style={{ width: '100%', maxWidth: '500px' }}>
+              <div>
+                {gambit && gambit.name != 'Proceed as Planned' ? (
+                  <>
+                    <h4>
+                      If Gambit successful add this to your score: <b> {Math.min(30, 50 - primaryScore)} VP</b>
+                    </h4>
+                    <h3>Gambit</h3>
+                    <h4>{gambit.name}</h4>
+                    <p>
+                      <strong>Gambit Mission:</strong> <br />
+                      {gambit.gambit}
+                    </p>
+                  </>
+                ) : (
+                  <>
+                    <h3>Primary Mission</h3>
+                    <h4>{store.mission.name}</h4>
+                    <p>
+                      <strong>Primary Mission Rule:</strong> <br />
+                      {store.mission.special}
+                    </p>
+                    <p>
+                      <strong>Scoring for current round:</strong> <br /> {store.mission.battle_rounds[turn]}
+                    </p>
+                  </>
+                )}
+              </div>
+              <div>
+                <h4>
+                  <strong>Mission Rule</strong>
+                </h4>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+                  {store.rule.map((mission, index) => {
+                    return (
+                      <div key={index} style={{ border: '1px solid white', padding: '20px', flex: '1' }}>
+                        <h3>{mission.name}</h3>
+                        <div style={{ fontSize: '12px' }}>
+                          <p>{mission.rule}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <h3>Deployment</h3>
+                <img style={{ maxWidth: '300px' }} alt="Deployment Zone" src={store.deployment.image} />
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}</>)}
     </div>
   );
 }
